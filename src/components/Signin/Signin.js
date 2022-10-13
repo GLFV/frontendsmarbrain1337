c29bfc674122565aa0088d9853d93f6be4e32376
Олександр <90966662+GLFV@users.noreply.github.com>
2022-10-13 23:32:29 +0300

    second

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signIn', {
+		fetch('https://smarbrainbackend1337.herokuapp.com/signIn', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
